1eeac118528b7942769d1851a0b8edeb5ee747a9
Christian <122573853+MyTechDevice@users.noreply.github.com>
2025-02-12 21:12:17 +0100

    new file:   .editorconfig 	modified:   .gitignore 	modified:   angular.json 	modified:   e2e/src/app.e2e-spec.ts 	modified:   e2e/tsconfig.json 	modified:   package.json 	modified:   src/app/app.component.css 	modified:   src/app/app.component.ts 	new file:   src/app/home/home.component.css 	new file:   src/app/home/home.component.ts 	new file:   src/app/housing-location/housing-location.component.css 	new file:   src/app/housing-location/housing-location.component.ts 	modified:   src/favicon.ico 	modified:   src/index.html 	modified:   src/main.ts 	modified:   src/styles.css 	modified:   tsconfig.app.json 	modified:   tsconfig.json 	modified:   tsconfig.spec.json

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,11 +1,24 @@
 import { browser, element, by, logging } from 'protractor';
 
-describe('first-app-lesson-01 app', () => {
+describe('first-app-lesson-03 app', () => {
 
   beforeEach(() => browser.get(''));
 
   it('should display correct title', async () => {
-    expect(await element.all(by.css('h1')).get(0).getText()).toEqual('Hello world!');
+    expect(await element.all(by.css('.brand-logo')).get(0).getAttribute('src')).toContain('/assets/logo.svg');
+  });
+
+  it('should have a filter string input', async () => {
+    expect(await element.all(by.css('input')).get(0).getAttribute('placeholder')).toEqual('Filter by city');
+    expect(await element.all(by.css('input')).get(0).getAttribute('type')).toEqual('text');
+  });
+
+  it('should have a search button', async () => {
+    expect(await element.all(by.css('button')).get(0).getText()).toEqual('Search');
+  });
+
+  it('should have a template housing-location component', async () => {
+    expect(await element.all(by.css('app-housing-location > p')).get(0).getText()).toEqual('housing-location works!');
   });
 
   afterEach(async () => {
